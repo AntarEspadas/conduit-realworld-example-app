c07a1d98c0066f76f8c6a4bf12e1b41cbe8c130f
Antar Espadas <antar.espadas@hotmail.com>
2025-07-15 21:01:46 -0600

    Traducir página principal a español

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
       {!isAuth && (
         <BannerContainer>
           <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
+          <p>Un lugar donde compartir tu conocimiento</p>
         </BannerContainer>
       )}
       <ContainerRow type="page">
